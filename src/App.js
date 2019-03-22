7f0a8c20fd1273adcd366a1a0034fc61afd536e4
Jaxon Porter <jaxonutah@gmail.com>
2019-03-22 14:58:30 -0600

    added redux, semanitic, and home

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, } from "react-router-dom";
+import Home from './components/Home';
+import { Container, } from 'semantic-ui-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => ( 
+  <Container>
+    <Route exact path="/" component={Home} />
+  </Container>
+);
+
 
 export default App;
+
